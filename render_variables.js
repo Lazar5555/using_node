973295ebac73935abb24fae7ecfc81caebccc9fb
Eleazar Ponce <poncebe@gmail.com>
2017-08-16 16:59:32 -0500

    NodeJS W3School course started.

--- render_variables.js
+++ render_variables.js
@@ -7,12 +7,12 @@ http.createServer(function(req, res){
     var variables = html_string.match(/[^\{\}]+(?=\})/g);
     var name = "Eleazar Ponce"
 
-    for(var = variables.length-1; i >= 0; i--){
+    for(var i = variables.length-1; i >= 0; i--){
       var value = eval(variables[i]);
       html_string = html_string.replace("{"+variables[i]+"}", value);
-    }
+    };
 
-    res.write(html);
+    res.write(html_string);
     res.end();
   });
 }).listen(8080);
